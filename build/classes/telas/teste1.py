8910c4fc34a9bec7e5860f584fcda6d94126c734
Adriano <adriano.fonseca1000@gmail.com>
2019-10-03 21:45:59 -0300

    Initial commit

---
+++ build/classes/telas/teste1.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Adriano Fonseca"
+__date__ = "$19/03/2016 13:03:02$"
+
+if __name__ == "__main__":
+    print "Hello World";
